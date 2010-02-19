92d2685a945828e9bef6f314d9e5bf37dbad10bc
Jason Tsao <jtsao22@gmail.com>
2010-02-19 14:55:20 -0800

    This bash script simply uploads the code to the micazs with their respective ids and ip addresses.

---
+++ make_for_all_micaz.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+make micaz install,1 eprb,172.17.4.170
+make micaz install,2 eprb,172.17.4.171
+make micaz install,6 eprb,172.17.4.175
+make micaz install,7 eprb,172.17.4.176
+make micaz install,8 eprb,172.17.4.177
+make micaz install,11 eprb,172.17.4.180
+make micaz install,12 eprb,172.17.4.181
+make micaz install,16 eprb,172.17.4.185
+make micaz install,17 eprb,172.17.4.186
